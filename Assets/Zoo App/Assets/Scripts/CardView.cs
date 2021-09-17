3bdff07d71dbef3cab9c1aa75cc7fad889db4754
sj-jason-liu <sj.jason.liu@gmail.com>
2021-09-17 23:09:11 +0800

    Finished creating zoo map objects.

---
+++ Assets/Zoo App/Assets/Scripts/CardView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+    public Image animalImage;
+
+    public CardModel[] card;
+
+    public void SelectCart(int cardIndex)
+    {
+        title.text = card[cardIndex].title;
+        description.text = card[cardIndex].description;
+        exhibit.text = card[cardIndex].exhibit;
+        animalImage.sprite = card[cardIndex].animalImage;
+    }
+}
