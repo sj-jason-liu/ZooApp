e93f58070ca0d18ea262075c6db9c879dc926749
sj-jason-liu <sj.jason.liu@gmail.com>
2021-09-18 22:28:26 +0800

    Finished setting Analytics on animals button clicking(need to check the result after hours) & still struggleing on app resolution on mobile, and need to fix for a better use.

---
+++ Assets/Zoo App/Assets/Scripts/AnalyticClicking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticClicking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClickEvent()
+    {
+
+        AnalyticsResult result = AnalyticsEvent.Custom("Button Clicked", new Dictionary<string, object>
+        {
+            {"selected_animal", gameObject.name },
+            {"time_elapsed", Time.timeSinceLevelLoad }
+        });
+        Debug.Log(gameObject.name + " clicked!");
+        Debug.Log("Analytics result: " + result);
+    }
+}
